4fd6e34b32067589009ee0f08899b38b1d33aaf1
teja.ct <teja.tummalapalli>
2016-10-24 11:12:01 -0400

    Mobile cloud backend in node.js

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mobile Cloud Backend with Node.js
+To deploy your simple backend, node.js application, please click the "Deploy to Bluemix" button below.
+
+[![Deploy to Bluemix](https://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/snippet-java/mobile_cloud_nodejs_backend.git)
---
+++ api.js
@@ -0,0 +1,51 @@
+var appRouter = function(app) {
+    
+    
+    /*Home page of the app*/
+    app.get("/",function(req,res) {
+        res.send("hello! , This is nodeJs backend for mobile cloud app. Post to /checklogin with username and password.")
+    });
+    
+
+   /*
+    Function to validate the user credentials
+
+    This function is accessible by the /checklogin URI, relative to the server ip address
+
+    The function accepts a "Request" object  sent by any client by POST method. The user credential
+    is passed in the body of the request as JSON object.
+
+    The function checks the user credential and prepares another JSON object with required data for 
+    client-side processing.
+   */
+   
+
+   app.post( "/checklogin", function( req, res ){
+
+      console.log( "Request received. Checking...." )
+
+      //if the request does not contain username
+      // or password, the server cannot do much, can it?
+      if( !req.body.user || 
+          !req.body.password ){
+          console.log( "Did not receive any credential" );          
+          return res.send( JSON.stringify( {"result":"error", "description" : "Credential missing" } ) );
+      }
+      //This condition simply validates the user name and
+      //password against the actual credentials stored in environment variables
+      else if( req.body.user == process.env.APP_USER_NAME &&
+          req.body.password == process.env.APP_USER_PASSWORD ){
+          //send a success message and also the record shows
+          //that the student has great record
+          return res.send(JSON.stringify( {"result":"success", "record":"You are outstanding" } ) );
+
+      }
+      else{
+          //well, user name and password did not match the actual credentials,
+          //it seems the user is not our student
+          //Send error message with the reason of failure.
+          return res.send( JSON.stringify( {"result": "error", "description": "Incorrect credentials" } ) );
+      }      
+   }); 
+}
+ 
